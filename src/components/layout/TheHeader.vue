cf390695b8eba401e7c18a2f589bb8d8e3b88b47
Kateryna Tolok <katerynatolok@Kates-MacBook-Pro.fritz.box>
2025-05-16 19:52:44 +0200

    create file postcss.config.mjs

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,35 +1,37 @@
 <template>
   <header class="bg-black text-white py-4">
-    <div class="container mx-auto flex items-center justify-between">
+    <div class="container mx-auto flex items-center">
       <button
         @click="toggleMenu"
         class="w-6 h-6 flex flex-col justify-between cursor-pointer focus:outline-none"
       >
         <span
-          class="block w-full h-0.5 bg-white transition-all duration-300"
-          :class="{ 'rotate-45 translate-y-2.5': isMenuOpen }"
+          class="block h-0.5 bg-white transition-transform duration-300"
+          :class="isMenuOpen ? 'rotate-45 translate-y-2' : ''"
         ></span>
         <span
-          class="block w-full h-0.5 bg-white transition-opacity duration-300"
-          :class="{ 'opacity-0': isMenuOpen }"
+          class="block h-0.5 bg-white transition-opacity duration-300"
+          :class="isMenuOpen ? 'opacity-0' : 'opacity-100'"
         ></span>
         <span
-          class="block w-full h-0.5 bg-white transition-all duration-300"
-          :class="{ '-rotate-45 -translate-y-2.5': isMenuOpen }"
+          class="block h-0.5 bg-white transition-transform duration-300"
+          :class="isMenuOpen ? '-rotate-45 -translate-y-2' : ''"
         ></span>
       </button>
 
-      <nav class="hidden md:flex space-x-4">
+      <nav :class="['md:flex space-x-4', isMenuOpen ? 'block' : 'hidden']">
         <BaseLink to="/">Games</BaseLink>
         <BaseLink to="/sports">Sports</BaseLink>
         <BaseLink to="/promotion">Promotion</BaseLink>
         <BaseLink to="/live-casino">Live Casino</BaseLink>
       </nav>
 
-      <div class="flex items-center">
-        <img src="../../imgs/logo.jpg" alt="mega rich Logo" class="h-8 mr-4" />
-        <button class="mr-4"></button>
+      <div class="flex-grow flex justify-center">
+        <img src="../../imgs/logo.jpg" alt="mega rich Logo" class="w-[130px] h-[110px] mr-4" />
+      </div>
 
+      <div class="flex items-center">
+        <button class="mr-4"></button>
         <BaseButton theme="secondary" class="hidden md:block mr-2">Sign In</BaseButton>
         <BaseButton theme="primary" class="hidden md:block">Sign Up</BaseButton>
       </div>
