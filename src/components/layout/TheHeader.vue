7800e26831a54fbfd9dee896c3c50a3de39b731e
Kateryna Tolok <katerynatolok@Kates-MacBook-Pro.fritz.box>
2025-05-16 19:00:00 +0200

    add logo in TheHeader.vue.And import this component to App.vue

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,8 +1,22 @@
 <template>
   <header class="bg-black text-white py-4">
     <div class="container mx-auto flex items-center justify-between">
-      <button @click="toggleMenu" class="text-xl mr-4 focus:outline-none md:hidden cursor-pointer">
-        - -
+      <button
+        @click="toggleMenu"
+        class="w-6 h-6 flex flex-col justify-between cursor-pointer focus:outline-none"
+      >
+        <span
+          class="block w-full h-0.5 bg-white transition-all duration-300"
+          :class="{ 'rotate-45 translate-y-2.5': isMenuOpen }"
+        ></span>
+        <span
+          class="block w-full h-0.5 bg-white transition-opacity duration-300"
+          :class="{ 'opacity-0': isMenuOpen }"
+        ></span>
+        <span
+          class="block w-full h-0.5 bg-white transition-all duration-300"
+          :class="{ '-rotate-45 -translate-y-2.5': isMenuOpen }"
+        ></span>
       </button>
 
       <nav class="hidden md:flex space-x-4">
@@ -13,7 +27,7 @@
       </nav>
 
       <div class="flex items-center">
-        <img src="" alt="mega rich Logo" class="h-8 mr-4" />
+        <img src="../../imgs/logo.jpg" alt="mega rich Logo" class="h-8 mr-4" />
         <button class="mr-4"></button>
 
         <BaseButton theme="secondary" class="hidden md:block mr-2">Sign In</BaseButton>
