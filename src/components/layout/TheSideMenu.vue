696647b65f77fc010daf6e23fdfd2d1e4c064ee1
Kateryna Tolok <katerynatolok@Kates-MacBook-Pro.fritz.box>
2025-05-18 02:01:20 +0200

    set black background for side menu. Updated TheSideMenu.vue to have a black background andtext-yellow-500

--- src/components/layout/TheSideMenu.vue
+++ src/components/layout/TheSideMenu.vue
@@ -1,18 +1,20 @@
 <template>
-  <transition name="slide">
-    <aside v-if="isOpen" class="fixed inset-0 z-50 bg-white w-64 shadow-lg">
+  <transition name="slide" class="bg-black text-white">
+    <aside v-if="isOpen" class="fixed inset-0 z-50 bg-black text-white w-64 shadow-lg">
       <button
         @click="handleClose"
-        class="absolute top-4 right-4 text-gray-600 hover:text-black text-2xl"
+        class="absolute top-4 right-4 text-yellow-500 hover:text-yellow-700 text-2xl"
         aria-label="Close menu"
       >
         &times;
       </button>
       <nav class="mt-16 px-4">
         <ul class="space-y-4">
-          <li><a href="/" class="text-lg text-gray-800 hover:text-blue-500">Home</a></li>
-          <li><a href="/about" class="text-lg text-gray-800 hover:text-blue-500">About</a></li>
-          <li><a href="/contact" class="text-lg text-gray-800 hover:text-blue-500">Contact</a></li>
+          <li><a href="/" class="text-lg text-yellow-500 hover:text-yellow-600">Home</a></li>
+          <li><a href="/about" class="text-lg text-yellow-500 hover:text-yellow-600">About</a></li>
+          <li>
+            <a href="/contact" class="text-lg text-yellow-500 hover:text-yellow-600">Contact</a>
+          </li>
         </ul>
       </nav>
     </aside>
