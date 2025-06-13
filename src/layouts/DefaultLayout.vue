1263b71e7957369a16c8ef747db58dd79b72eec4
Kateryna Tolok <katerynatolok@Kates-MacBook-Pro.fritz.box>
2025-06-13 11:37:26 +0200

    Integrate BasePopup component. This commit introduces the  component into the main application layout ( based on context). - Imported and added the  component directly to the template.

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -4,6 +4,7 @@ import { ref, watch } from 'vue'
 import TheHeader from '@/components/layout/TheHeader.vue'
 import TheSideMenu from '@/components/layout/TheSideMenu.vue'
 import TheFooter from '@/components/layout/TheFooter.vue'
+import BasePopup from '@/components/base/BasePopup.vue'
 
 const isMenuOpen = ref(false)
 
@@ -24,6 +25,8 @@ watch(
 </script>
 
 <template>
+  <BasePopup />
+
   <TheHeader @open-side-menu="openSideMenu" :isMenuOpen="isMenuOpen" />
 
   <router-view />
