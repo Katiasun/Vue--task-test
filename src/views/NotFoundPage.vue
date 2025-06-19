6397771c4896fcd4138ff8033040005a5d656e47
Kateryna Tolok <katerynatolok@Kates-MacBook-Pro.fritz.box>
2025-06-19 20:20:58 +0200

    Specifically:
    Added a blank line between the h1 and p tags within the error message block.
    Added a blank line after the text in the BaseButton component.

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -16,11 +16,12 @@ function gotToHome() {
   >
     <div class="mx-w-xl p-10 bg-gray rounded-xl shadow-lg text-center mb-8">
       <h1 class="text-5xl md-text-9xl font-extrabold text-btn-primary-default mb-0">Error 404!</h1>
+
       <p class="text-2xl md:text-3xl mt-2 mb-4">
         Sorry, the page you are looking for doesn't exist.
       </p>
     </div>
 
-    <BaseButton @click="gotToHome" theme="primary">Return to home page</BaseButton>
+    <BaseButton @click="gotToHome" theme="primary"> Return to home page </BaseButton>
   </div>
 </template>
